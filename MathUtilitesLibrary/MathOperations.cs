4fa0f07f6c8c29c644e174b4973b5c300a6c7954
Shadvonic <moxeymarcus28@gmail.com>
2025-03-09 14:17:46 -0400

    Add project files.

---
+++ MathUtilitesLibrary/MathOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUtilitiesLibrary
+{
+    public static class MathOperations
+    {
+        public static double Add(double x, double y)
+        {
+            double output = 0;
+            output = x + y; 
+            return output;
+        }
+
+        public static double Subtract(double x, double y)
+        {
+
+            double output = 0;
+            output = x - y;
+            return output;
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            double output = 0;
+            output = x * y;
+            return output;
+
+        }
+
+        public static double Divide(double x, double y) 
+        {
+
+            double output = 0;
+            output = x / y;
+            return output;
+        }
+    }
+}
